a66ac081c2a3bea2c7dd58f8e9a561a8b8652f9a
Vitor Oliveira <vitorsimon04@gmail.com>
2025-02-18 16:39:39 -0300

    FEAT - Melhoria do models de Usuário

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -11,19 +11,38 @@ const Usuario = sequelize.define('Usuario', {
   },
   nome: {
     type: DataTypes.STRING(100),
-    allowNull: false
+    allowNull: false,
+    validate: {
+      len: [3, 100]
+    }
   },
   email: {
     type: DataTypes.STRING(100),
     allowNull: false,
-    unique: true
+    unique: {
+      args: true,
+      msg: 'E-mail já cadastrado!'
+    },
+    validate: {
+      isEmail: {
+        msg: 'E-mail inválido!'
+      }
+    }
   },
   senha: {
     type: DataTypes.STRING(255),
-    allowNull: true
+    allowNull: false
+  },
+  telefone: {
+    type: DataTypes.STRING(20),
+    allowNull: true,
+    validate: {
+      is: /^[0-9()+\- ]*$/, // Permite apenas números, parênteses, traços e espaços
+      len: [8, 20]
+    }
   },
   status: {
-    type: DataTypes.STRING(20),
+    type: DataTypes.ENUM('ativo', 'inativo', 'pendente'),
     allowNull: false,
     defaultValue: 'ativo'
   },
