3d82eaa1e755eadfd369f539935d1eb7308c98eb
Vitor Oliveira <vitorsimon04@gmail.com>
2025-02-19 16:43:11 -0300

    FEAT - Adição de retorno de códigos 400 no errorhandler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,14 @@
 function errorHandler(err, req, res, next) {
     console.error(`[Erro] ${err.message} - Rota: ${req.originalUrl}`);
 
+    if (err.status === 400) {
+        return res.status(400).json({ erro: err.message });
+    }
+
+    if (err.status === 404) {
+        return res.status(404).json({ erro: err.message });
+    }
+
     const statusCode = err.statusCode || 500;
     return res.status(statusCode).json({
         erro: err.message || 'Erro interno do servidor',
