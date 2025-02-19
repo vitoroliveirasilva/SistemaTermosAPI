1dc15c52e8e5e15dcea9b56c4ced237cb336f3d9
Vitor Oliveira <vitorsimon04@gmail.com>
2025-02-19 00:43:44 -0300

    FIX - Correção e melhoria do controller do Usuário

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,66 +1,64 @@
 const usuarioService = require('../services/usuarioService');
 
-async function criarUsuario(req, res, next) {
-  try {
-    const usuario = await usuarioService.cadastrarUsuario(req.body);
-    return res.status(201).json(usuario);
-  } catch (error) {
-    next(error);
+class UsuarioController {
+  async criar(req, res, next) {
+    try {
+      const usuario = await usuarioService.criar(req.body);
+      return res.status(201).json(usuario);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async listar(req, res, next) {
+    try {
+      const usuarios = await usuarioService.listar();
+      return res.json(usuarios);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async buscarPorFiltros(req, res, next) {
+    try {
+      const usuarios = await usuarioService.listar(req.query);
+      return res.json(usuarios);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async buscarPorId(req, res, next) {
+    try {
+      const usuario = await usuarioService.buscarPorId(req.params.id);
+      if (!usuario) {
+        return res.status(404).json({
+          erro: 'Usuário não encontrado'
+        });
+      }
+      return res.json(usuario);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async atualizar(req, res, next) {
+    try {
+      const usuario = await usuarioService.atualizar(req.params.id, req.body);
+      return res.json(usuario);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async remover(req, res, next) {
+    try {
+      await usuarioService.remover(req.params.id);
+      return res.status(204).send();
+    } catch (error) {
+      next(error);
+    }
   }
 }
 
-async function listarUsuarios(req, res, next) {
-  try {
-    const usuarios = await usuarioService.listarUsuarios();
-    return res.json(usuarios);
-  } catch (error) {
-    next(error);
-  }
-}
-
-async function obterUsuario(req, res, next) {
-  try {
-    const {
-      id
-    } = req.params;
-    const usuario = await usuarioService.obterUsuario(id);
-    return res.json(usuario);
-  } catch (error) {
-    next(error);
-  }
-}
-
-async function atualizarUsuario(req, res, next) {
-  try {
-    const {
-      id
-    } = req.params;
-    const usuario = await usuarioService.editarUsuario(id, req.body);
-    return res.json(usuario);
-  } catch (error) {
-    next(error);
-  }
-}
-
-async function deletarUsuario(req, res, next) {
-  try {
-    const {
-      id
-    } = req.params;
-    const usuarioRemovido = await usuarioService.removerUsuario(id);
-    return res.json({
-      message: 'Usuário desativado com sucesso',
-      usuario: usuarioRemovido
-    });
-  } catch (error) {
-    next(error);
-  }
-}
-
-module.exports = {
-  criarUsuario,
-  listarUsuarios,
-  obterUsuario,
-  atualizarUsuario,
-  deletarUsuario
-};
+module.exports = new UsuarioController();
