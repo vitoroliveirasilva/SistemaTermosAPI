5ef14bd00ad0b61b98abfc300084b7388c7ecf42
Vitor Oliveira <vitorsimon04@gmail.com>
2025-02-18 23:04:36 -0300

    FEAT - Adição das rotas de filias no index do routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 // Importação centralizada das rotas
 const usuarioRoutes = require('./usuarioRoutes');
+const filialRoutes = require('./filialRoutes');
 
 // Definição das rotas
 router.use('/usuarios', usuarioRoutes);
+router.use('/filiais', filialRoutes);
 
 module.exports = router;
