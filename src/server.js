57772a66c9ae30fbb595b4712c1cf4a3954fd06b
Vitor Oliveira <vitorsimon04@gmail.com>
2025-02-19 13:51:45 -0300

    FIX - Correção de console.log para informar que o servidor está rodando

--- src/server.js
+++ src/server.js
@@ -9,10 +9,7 @@ const PORT = process.env.PORT || 3000;
 async function startServer() {
   try {
     await syncDatabase(); // Sincroniza banco de dados
-
-    app.listen(PORT, () => {
-      console.log(`Servidor rodando na porta ${PORT}`);
-    });
+    app.listen(PORT, () => console.info(`Servidor rodando na porta ${PORT}`));
   } catch (error) {
     console.error('Erro ao iniciar o servidor:', error);
     process.exit(1);
