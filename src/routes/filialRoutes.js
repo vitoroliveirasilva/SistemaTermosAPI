ed681ae83a8993218f61064ac8593f9be8c7ae43
Vitor Oliveira <vitorsimon04@gmail.com>
2025-02-19 00:38:15 -0300

    FEAT - Criação de rota para aplicação de filtros sobre listagem de usuários e filiais

--- src/routes/filialRoutes.js
+++ src/routes/filialRoutes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const filialController = require('../controllers/filialController');
 const {
-    validarId
+    validarId,
+    validarQueryParamsFiliais
 } = require('../middlewares');
 
 router.post('/', filialController.criar);
 router.get('/', filialController.listar);
+router.get('/filtros', validarQueryParamsFiliais, filialController.buscarPorFiltros);
 router.get('/:id', validarId, filialController.buscarPorId);
 router.put('/:id', validarId, filialController.atualizar);
 router.delete('/:id', validarId, filialController.remover);
--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
 const {
-    validarId
+    validarId,
+    validarQueryParamsUsuarios
 } = require('../middlewares');
 
 router.post('/', usuarioController.criar);
 router.get('/', usuarioController.listar);
+router.get('/filtros', validarQueryParamsUsuarios, usuarioController.buscarPorFiltros);
 router.get('/:id', validarId, usuarioController.buscarPorId);
 router.put('/:id', validarId, usuarioController.atualizar);
 router.delete('/:id', validarId, usuarioController.remover);
