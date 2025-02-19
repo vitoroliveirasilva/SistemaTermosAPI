7fd3a4b8cdc1178b9b30b0b95e3b34adf52383a8
Vitor Oliveira <vitorsimon04@gmail.com>
2025-02-18 23:27:41 -0300

    FEAT - Adição de busca por filtros dos atributos de Usuário

--- src/repositories/usuarioRepository.js
+++ src/repositories/usuarioRepository.js
@@ -1,49 +1,90 @@
 const {
   Usuario
 } = require('../models');
+const {
+  Op
+} = require('sequelize');
 
-async function criarUsuario(dados) {
-  return await Usuario.create(dados);
-}
 
-async function buscarUsuarioPorId(id) {
-  return await Usuario.findByPk(id);
-}
+class UsuarioRepository {
+  async criar(dados) {
+    return await Usuario.create(dados);
+  }
 
-async function buscarUsuarioPorEmail(email) {
-  return await Usuario.findOne({
-    where: {
+  async listar(filtros = {}) {
+    return await Usuario.findAll({
+      where: this.#construirFiltros(filtros)
+    });
+  }
+
+  async buscarPorId(id) {
+    return await Usuario.findByPk(id);
+  }
+
+  async buscarPorFiltros({
+    nome,
+    status,
+    email,
+    incluirSenha = false
+  }) {
+    const filtros = this.#construirFiltros({
+      nome,
+      status,
       email
+    });
+
+    return await Usuario.scope(incluirSenha ? 'withPassword' : null).findAll({
+      where: filtros
+    });
+  }
+
+  async atualizar(id, dados) {
+    const usuario = await this.buscarPorId(id);
+    if (!usuario) return null;
+    return await usuario.update(dados);
+  }
+
+  async remover(id) {
+    const usuario = await this.buscarPorId(id);
+    if (!usuario) return null;
+
+    await usuario.update({
+      status: 'inativo'
+    });
+    return usuario;
+  }
+
+  /*
+   * Método privado para construir filtros dinâmicos
+   * @param {Object} filtros - Objeto contendo os filtros (nome, status, email)
+   * @returns {Object} - Objeto de filtros formatado para Sequelize
+  */
+  #construirFiltros({
+    nome,
+    status,
+    email
+  }) {
+    const filtros = {};
+
+    if (nome) {
+      filtros.nome = {
+        [Op.like]: `%${nome}%`
+      };
     }
-  });
+
+    if (email) {
+      filtros.email = {
+        [Op.like]: `%${email}%`
+      };
+    }
+
+    if (status) {
+      filtros.status = status;
+    }
+
+    return filtros;
+  }
+
 }
 
-async function listarUsuarios() {
-  return await Usuario.findAll();
-}
-
-async function atualizarUsuario(id, dados) {
-  const usuario = await Usuario.findByPk(id);
-  if (!usuario) return null;
-  return await usuario.update(dados);
-}
-
-async function deletarUsuario(id) {
-  const usuario = await Usuario.findByPk(id);
-  if (!usuario) return null;
-
-  // Alterando status para inativo
-  await usuario.update({
-    status: 'inativo'
-  });
-  return usuario;
-}
-
-module.exports = {
-  criarUsuario,
-  buscarUsuarioPorId,
-  buscarUsuarioPorEmail,
-  listarUsuarios,
-  atualizarUsuario,
-  deletarUsuario
-};
+module.exports = new UsuarioRepository();
