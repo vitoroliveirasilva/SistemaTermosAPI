8c624a46ac0f61a73f20aeafde14c2dfbadd5dfa
Vitor Oliveira <vitorsimon04@gmail.com>
2025-02-19 10:57:13 -0300

    FEAT - Criação de schema para atualização de Usuário

--- src/validations/usuarioSchema.js
+++ src/validations/usuarioSchema.js
@@ -50,6 +50,44 @@ const usuarioSchema = Joi.object({
         })
 });
 
+const usuarioUpdateSchema = Joi.object({
+    nome: Joi.string()
+        .min(3)
+        .max(100)
+        .pattern(regexNome)
+        .messages({
+            'string.base': 'Nome deve ser uma string válida.',
+            'string.min': 'Nome deve ter pelo menos 3 caracteres.',
+            'string.max': 'Nome deve ter no máximo 100 caracteres.',
+            'string.pattern.base': 'Nome deve conter apenas letras, espaços e acentos.',
+        }),
+
+
+    email: Joi.string()
+        .email({
+            tlds: {
+                allow: false
+            }
+        })
+        .messages({
+            'string.email': 'E-mail inválido.',
+        }),
+
+    senha: Joi.string()
+        .pattern(regexSenha)
+        .messages({
+            'string.pattern.base': 'A senha deve ter pelo menos 8 caracteres, incluindo: uma maiúscula, uma minúscula, um número e um caractere especial (@$!%*?&).',
+        }),
+
+    status: Joi.string()
+        .valid(...statusValores)
+        .messages({
+            'any.only': `O status deve ser um dos seguintes valores: ${statusValores.join(', ')}.`,
+        })
+}).min(1); // Garante que pelo menos um campo seja enviado na atualização
+
+
 module.exports = {
-    usuarioSchema
+    usuarioSchema,
+    usuarioUpdateSchema
 };
