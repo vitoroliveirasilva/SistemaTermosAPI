71c7866edd851a9f5f98ce524417f22eae72e054
Vitor Oliveira <vitorsimon04@gmail.com>
2025-02-20 18:39:36 -0300

    REFACT - Indentação do código

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,11 +2,15 @@ function errorHandler(err, req, res, next) {
     console.error(`[Erro] ${err.message} - Rota: ${req.originalUrl}`);
 
     if (err.status === 400) {
-        return res.status(400).json({ erro: err.message });
+        return res.status(400).json({
+            message: err.message
+        });
     }
 
     if (err.status === 404) {
-        return res.status(404).json({ erro: err.message });
+        return res.status(404).json({
+            message: err.message
+        });
     }
 
     const statusCode = err.statusCode || 500;
