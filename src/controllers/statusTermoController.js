30cd6aaeb5fd30e51a75fbf7fd293c00497a2508
Vitor Oliveira <vitorsimon04@gmail.com>
2025-02-21 01:30:29 -0300

    FEAT - Criação de controller para status dos termos

---
+++ src/controllers/statusTermoController.js
@@ -0,0 +1,59 @@
+const statusTermoService = require('../services/statusTermoService');
+
+class StatusTermoController {
+    async criar(req, res, next) {
+        try {
+            const statusTermo = await statusTermoService.criar(req.body);
+            return res.status(201).json(statusTermo);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async listar(req, res, next) {
+        try {
+            const statusTermos = await statusTermoService.listar();
+            return res.json(statusTermos);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async buscarPorFiltros(req, res, next) {
+        try {
+            const statusTermos = await statusTermoService.listar(req.query);
+            return res.json(statusTermos);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async buscarPorId(req, res, next) {
+        try {
+            const statusTermo = await statusTermoService.buscarPorId(req.params.id);
+            return res.json(statusTermo);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async atualizar(req, res, next) {
+        try {
+            const statusTermo = await statusTermoService.atualizar(req.params.id, req.body);
+            return res.json(statusTermo);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async remover(req, res, next) {
+        try {
+            const resultado = await statusTermoService.remover(req.params.id);
+            return res.status(200).json(resultado);
+        } catch (error) {
+            next(error);
+        }
+    }
+}
+
+module.exports = new StatusTermoController();
