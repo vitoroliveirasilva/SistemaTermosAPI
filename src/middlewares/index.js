b987ad86f6eeaf74832ee6e1ba889356568172fb
Vitor Oliveira <vitorsimon04@gmail.com>
2025-02-21 01:30:03 -0300

    FEAT - Criação de middleware para validação de param de StatusTermo na query

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,11 +1,13 @@
 const validarIds = require('./validacao/validarIdsMiddleware');
 const validarQueryParamsUsuarios = require('./validacao/validarQueryParamsUsuariosMiddleware');
 const validarQueryParamsFiliais = require('./validacao/validarQueryParamsFiliaisMiddleware');
+const validarQueryParamsStatusTermo = require('./validacao/validarQueryParamsStatusTermoMiddleware');
 const errorHandler = require('./errorHandler');
 
 module.exports = {
   validarIds,
   validarQueryParamsUsuarios,
   validarQueryParamsFiliais,
+  validarQueryParamsStatusTermo,
   errorHandler
 };
