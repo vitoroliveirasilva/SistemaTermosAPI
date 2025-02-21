5036602022d4f42d13f0e831aa5e9c2ade9153c7
Vitor Oliveira <vitorsimon04@gmail.com>
2025-02-21 01:31:25 -0300

    FEAT - Criação de Schema para validação de status de termos

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,7 +1,9 @@
 const filialSchema = require('./filialSchema');
+const statusTermoSchema = require('./statusTermoSchema');
 const usuarioSchema = require('./usuarioSchema');
 
 module.exports = {
     filialSchema,
+    statusTermoSchema,
     usuarioSchema
 };
---
+++ src/validations/statusTermoSchema.js
@@ -0,0 +1,11 @@
+const Joi = require('joi');
+
+const statusTermoSchema = Joi.object({
+    nome: Joi.string().min(2).max(100).required().messages({
+        "string.empty": "O nome do status do termo é obrigatório.",
+        "string.min": "O nome deve ter pelo menos 2 caracteres.",
+        "string.max": "O nome deve ter no máximo 100 caracteres."
+    })
+});
+
+module.exports = statusTermoSchema;
