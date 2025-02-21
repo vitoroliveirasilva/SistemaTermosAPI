f1f4b16df6a01683a5244f9f8d1deeb3b1e88f48
Vitor Oliveira <vitorsimon04@gmail.com>
2025-02-20 23:41:53 -0300

    FIX - Retorno apenas os erros ao invés de lançar erros filialUtils

--- src/utils/filialUtils.js
+++ src/utils/filialUtils.js
@@ -16,32 +16,32 @@ async function validarUnicidadeFilial(dados, id = null) {
     // Validação dos dados com base no schema JOI da filial
     validarDados(dados, filialSchema);
 
-    // Verifica o nome e endereço simultaneamente
-    const filiaisExistentes = await filialRepository.buscarPorFiltros(dados);
+    // Busca todas as filiais com o mesmo nome ou endereço
+    const filiaisExistentes = await filialRepository.buscarPorFiltros({
+        nome: dados.nome,
+        endereco: dados.endereco
+    });
 
     // Filtra os registros encontrados, ignorando o ID atual (só se houver um id pra ser filtrado)
     const filiaisFiltradas = filiaisExistentes.filter(filial => filial.id !== id);
 
-    // Verifica conflitos para nome e endereço
-    const nomeConflito = filiaisFiltradas.some(f => f.nome === nome);
-    const enderecoConflito = filiaisFiltradas.some(f => f.endereco === endereco);
-
-    // Define a mensagem de erro conforme os conflitos encontrados
-    let erroMsg = null;
-    if (nomeConflito && enderecoConflito) {
-        erroMsg = 'Nome e endereço já cadastrados.';
-    } else if (nomeConflito) {
-        erroMsg = 'Nome já cadastrado.';
-    } else if (enderecoConflito) {
-        erroMsg = 'Endereço já cadastrado.';
+    // Verifica conflitos
+    const erros = [];
+    if (filiaisFiltradas.some(f => f.nome === dados.nome)) {
+        erros.push({
+            campo: 'nome',
+            mensagem: 'Nome já cadastrado.'
+        });
     }
 
-    // Lança erro se houver conflito de dados
-    if (erroMsg) {
-        const error = new Error(erroMsg);
-        error.status = 400;
-        throw error;
+    if (filiaisFiltradas.some(f => f.endereco === dados.endereco)) {
+        erros.push({
+            campo: 'endereco',
+            mensagem: 'Endereço já cadastrado.'
+        });
     }
+
+    return erros;
 }
 
 module.exports = {
