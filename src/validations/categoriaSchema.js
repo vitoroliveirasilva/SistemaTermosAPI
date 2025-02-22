c0d6efa0c0fc28ff67e6c00abb497c20a5ebd4b6
Vitor Oliveira <vitorsimon04@gmail.com>
2025-02-21 22:08:18 -0300

    FEAT - Criação e importação do Schema JOI da Categoria

---
+++ src/validations/categoriaSchema.js
@@ -0,0 +1,15 @@
+const Joi = require('joi');
+
+const categoriaSchema = Joi.object({
+    nome: Joi.string().min(2).max(50).required().messages({
+        "string.empty": "O nome da categoria é obrigatório.",
+        "string.min": "O nome da categoria deve ter pelo menos 2 caracteres.",
+        "string.max": "O nome da categoria deve ter no máximo 50 caracteres."
+    }),
+    descricao: Joi.string().max(255).optional().messages({
+        "string.empty": "O nome da categoria é obrigatório.",
+        "string.max": "O nome da categoria deve ter no máximo 255 caracteres."
+    })
+});
+
+module.exports = categoriaSchema;
--- src/validations/index.js
+++ src/validations/index.js
@@ -1,9 +1,11 @@
 const filialSchema = require('./filialSchema');
 const statusTermoSchema = require('./statusTermoSchema');
+const categoriaSchema = require('./categoriaSchema');
 const usuarioSchema = require('./usuarioSchema');
 
 module.exports = {
     filialSchema,
     statusTermoSchema,
+    categoriaSchema,
     usuarioSchema
 };
