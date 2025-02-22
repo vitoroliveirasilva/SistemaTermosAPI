9b09f8ac417e4a6e0b244563575eb4cceeda5adc
Vitor Oliveira <vitorsimon04@gmail.com>
2025-02-22 00:26:15 -0300

    FEAT - Criação e importação do Schema JOI do EquipamentoStatus

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,11 +1,13 @@
 const filialSchema = require('./filialSchema');
 const statusTermoSchema = require('./statusTermoSchema');
+const statusEquipamentoSchema = require('./statusEquipamentoSchema');
 const categoriaSchema = require('./categoriaSchema');
 const usuarioSchema = require('./usuarioSchema');
 
 module.exports = {
     filialSchema,
     statusTermoSchema,
+    statusEquipamentoSchema,
     categoriaSchema,
     usuarioSchema
 };
---
+++ src/validations/statusEquipamentoSchema.js
@@ -0,0 +1,14 @@
+const Joi = require('joi');
+
+const statusEquipamentoSchema = Joi.object({
+    nome: Joi.string().min(2).max(50).required().messages({
+        "string.empty": "O nome do status é obrigatório.",
+        "string.min": "O nome deve ter pelo menos 2 caracteres.",
+        "string.max": "O nome deve ter no máximo 50 caracteres."
+    }),
+    descricao: Joi.string().max(255).optional().messages({
+        "string.max": "A descrição deve ter no máximo 255 caracteres."
+    })
+});
+
+module.exports = statusEquipamentoSchema;
