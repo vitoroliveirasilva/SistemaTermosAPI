5cf3c6a665ebb1d7d3f0979650efc9f3246a7fde
Vitor Oliveira <vitorsimon04@gmail.com>
2025-03-04 16:47:38 -0300

    FEAT - Criação de Controller da acaoMovimentacao

---
+++ src/controllers/acaoMovimentacaoController.js
@@ -0,0 +1,59 @@
+const acaoMovimentacaoService = require('../services/acaoMovimentacaoService');
+
+class AcaoMovimentacaoController {
+    async criar(req, res, next) {
+        try {
+            const acaoMovimentacao = await acaoMovimentacaoService.criar(req.body);
+            return res.status(201).json(acaoMovimentacao);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async listar(req, res, next) {
+        try {
+            const acoesMovimentacoes = await acaoMovimentacaoService.listar();
+            return res.json(acoesMovimentacoes);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async buscarPorFiltros(req, res, next) {
+        try {
+            const acoesMovimentacoes = await acaoMovimentacaoService.listar(req.query);
+            return res.json(acoesMovimentacoes);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async buscarPorId(req, res, next) {
+        try {
+            const acaoMovimentacao = await acaoMovimentacaoService.buscarPorId(req.params.id);
+            return res.json(acaoMovimentacao);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async atualizar(req, res, next) {
+        try {
+            const acaoMovimentacao = await acaoMovimentacaoService.atualizar(req.params.id, req.body);
+            return res.json(acaoMovimentacao);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    async remover(req, res, next) {
+        try {
+            const resultado = await acaoMovimentacaoService.remover(req.params.id);
+            return res.status(204).json(resultado);
+        } catch (error) {
+            next(error);
+        }
+    }
+}
+
+module.exports = new AcaoMovimentacaoController();
