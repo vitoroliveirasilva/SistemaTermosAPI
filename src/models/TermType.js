40f4dc7aad573c0ba347ba2a7b0c446de1ad0a2d
Vitor Oliveira <vitorsimon04@gmail.com>
2025-03-07 13:39:14 -0300

    FIX - Correção do model do TermType

--- src/models/TermType.js
+++ src/models/TermType.js
@@ -12,15 +12,34 @@ const TermType = sequelize.define('TermType', {
     nome: {
         type: DataTypes.STRING(50),
         allowNull: false,
-        unique: true
+        unique: {
+            args: true,
+            msg: 'Tipo de termo já cadastrado!'
+        },
+        validate: {
+            notEmpty: {
+                msg: "O nome do tipo de termo não pode ser vazio."
+            },
+            len: {
+                args: [2, 50],
+                msg: "O nome do tipo de termo deve ter entre 2 a 50 caracteres."
+            }
+        }
     },
-    criadoEm: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW
+    descricao: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+        validate: {
+            len: {
+                args: [0, 100],
+                msg: "A descrição do tipo de termo deve ter no máximo 255 caracteres."
+            }
+        }
     }
 }, {
     tableName: 'term_types',
-    timestamps: false
+    timestamps: true,
+    underscored: true
 });
 
 TermType.displayName = 'Tipo de termo';
