67c75c150ab896db9e84455ac90ad49b87db4e1e
Vitor Oliveira <vitorsimon04@gmail.com>
2025-04-15 17:00:19 -0300

    FEAT - Adição de cors, ratelimit, security e refatoração de app.js

--- src/app.js
+++ src/app.js
@@ -5,18 +5,26 @@ const {
   errorHandler
 } = require('./middlewares');
 
+// Configurações externas
+const cors = require('./config/cors');
+const rateLimit = require('./config/rateLimit');
+const applySecurity = require('./config/security');
+
 const app = express();
 
-// Middleware para leitura de JSON no body das requisições
+// Aplica configurações de segurança e limites de requisições
+app.use(rateLimit);
+app.use(cors);
 app.use(express.json());
+applySecurity(app);
 
 // Configuração do Swagger
 setupSwagger(app);
 
-// Definição das rotas principais
+// Rotas principais
 app.use('/api', routes);
 
-// Rota de teste
+// Rota de teste genérica
 app.get('/ping', (req, res) => res.send('pong'));
 
 // Middleware global para tratamento de erros
