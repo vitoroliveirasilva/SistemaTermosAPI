e512a2384ed142a141d545b629f9a394f4c36a72
Vitor Oliveira <vitorsimon04@gmail.com>
2025-05-17 01:09:36 -0300

    FIX - Correção de importação do validarUnicidadeEquipamentoStatus no equipamentoStatusService

--- src/services/equipamento/equipamentoStatusService.js
+++ src/services/equipamento/equipamentoStatusService.js
@@ -1,7 +1,7 @@
 const ServiceComValidacao = require('../validacaoService');
 const statusEquipamentoRepository = require('../../repositories/equipamento/equipamentoStatusRepository');
 const {
-    validarUnicidadeStatusEquipamento
+    validarUnicidadeEquipamentoStatus
 } = require('../../utils');
 
-module.exports = new ServiceComValidacao(statusEquipamentoRepository, 'Status', validarUnicidadeStatusEquipamento);
+module.exports = new ServiceComValidacao(statusEquipamentoRepository, 'Status', validarUnicidadeEquipamentoStatus);
